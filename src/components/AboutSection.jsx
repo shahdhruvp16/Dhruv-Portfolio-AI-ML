c6c8b163a8d330893fc71d7d354f96b88065ca68
Shah Dhruv Piyushkumar <128819773+shahdhruvp16@users.noreply.github.com>
2025-06-28 22:07:50 -0700

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -29,7 +29,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="https://drive.google.com/file/d/1aqeL9mhVeTuoFGErUPrEczcwzpeE-NIa/view"
+                href="https://drive.google.com/drive/home"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
